9d2328a19547dfd23b23b03c04ccfb37270b42e3
bek2518 <youbereketb20@@hotmail.com>
2022-06-30 06:44:41 -0700

    Print without printf, puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include<unistd.h>
 /**
  * main - entry point
  *
@@ -8,5 +8,6 @@ int main(void)
 {
 	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	
+	write(2, str1, 59);
 	return (1);
 }
