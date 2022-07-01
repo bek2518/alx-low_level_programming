0cf2bdf401933d04d27644c9fe468289ecacf723
bek2518 <youbereketb20@@hotmail.com>
2022-07-01 03:41:23 -0700

    prints all single digit numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Prints single digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
