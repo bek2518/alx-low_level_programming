6b78e7026e562879c004da2cd72f5566c14f993d
bek2518 <youbereketb20@@hotmail.com>
2022-07-01 03:30:44 -0700

    Prints alphabets except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alph[24] = "abcdfghijklmnoprstuvwxyz";
+	int i;
+
+	for (i = 0; i < 24; i++)
+	{
+		putchar(alph[i]);
+	}
+	putchar('\n');
+	return (0);
+}
