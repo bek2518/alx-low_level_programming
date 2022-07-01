03f8064bd058eb011b6fce690b4b02567391c3b7
bek2518 <youbereketb20@@hotmail.com>
2022-07-01 08:23:34 -0700

    Prints two digit numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - prints different combinations of two digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b;
+
+	a = 48;
+	b = 48;
+
+	while (b < 58)
+	{
+		a = 48;
+		while (a < 58)
+		{
+			if (b != a && b < a)
+			{
+				putchar(a);
+				putchar(b);
+				if (a == 57 && b == 56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			a++;
+		}
+		b++;
+	}
+	putchar('\n');
+	return (0);
+}
+
