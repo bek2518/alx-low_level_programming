65b73536f7a7b44394f21a15c56ffad8e1f0478d
bek2518 <youbereketb20@@hotmail.com>
2022-07-05 10:34:33 -0700

    Prints alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * prints the alphabets in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int print_alphabet(void)
+{
+	int i;
+
+	for( i = 97, i < 122, i++);
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
