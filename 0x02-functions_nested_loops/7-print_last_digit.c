b287d81b323235cb09b5f5f98de51e68d56f2ac3
bek2518 <youbereketb20@@hotmail.com>
2022-07-06 13:13:16 -0700

    Prints the last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,14 +10,14 @@ int print_last_digit(int i)
 	int l;
 	
 	l = i % 10;
-	if (i < 0)
-	{
-		_putchar(-l + 48);
-		return (-1);
-	}
-	else
+	if (i > 0)
 	{
 		_putchar(l + 48);
 		return (1);
+	}
+	else
+	{
+		_putchar(-l + 48);
+		return (-1);
 	}
 }
