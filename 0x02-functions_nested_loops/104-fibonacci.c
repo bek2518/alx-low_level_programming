af17c2187d04e2a216c91b66d7eaaad8f7611fa7
bek2518 <youbereketb20@@hotmail.com>
2022-07-09 03:21:00 -0700

    Finds and prints yje first 98 Fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,19 +8,19 @@
 int main(void)
 {
 	unsigned long int i, j, k, j1, j2, k1, k2;
-	
+
 	j = 1;
 	k = 2;
 
 	printf("%lu", j);
 
-	for (i = 1; i < 92; i++)
+	for (i = 1; i < 91; ++i)
 	{
 		printf(", %lu", k);
 		k = k + j;
 		j = k - j;
 	}
-	
+
 	j1 = j / 1000000000;
 	j2 = j % 1000000000;
 	k1 = k / 1000000000;
