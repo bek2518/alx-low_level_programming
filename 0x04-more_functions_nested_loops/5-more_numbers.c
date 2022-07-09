3f60c14a8af062494d8b92a5fe7b8a91d13e8ddd
bek2518 <youbereketb20@@hotmail.com>
2022-07-09 06:44:08 -0700

    Prints 0 to 14 ten times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,12 +7,20 @@
  */
 void more_numbers(void)
 {
-	int i, j;
+	int i, j, k;
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 14; j++)
-			_putchar(i + '0');
+		for (j = 0; j <= 9; j++)
+		{
+			if (j > 9)
+			{
+				k = j / 10;
+				_putchar(k + '0');
+			}
+			else
+				_putchar(j % 10 + '0');
+		}
+	_putchar('\n');
 	}
-	_putchar('0');
 }
