153711a8859499599f389eabcf56f9edb30aeb9b
bek2518 <youbereketb20@@hotmail.com>
2022-07-09 07:47:33 -0700

    Prints Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,6 +4,7 @@
  * main - prints Fizz for multiple of 3
  * prints buzz for multiples of 5
  * prints FizzBuzz for multiples of 3 and 5
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -12,6 +13,7 @@ int main(void)
 	{
 		a = n % 3;
 		b = n % 5;
+
 		if (a == 0 && b == 0)
 			printf("FizzBuzz");
 		else if (a == 0)
