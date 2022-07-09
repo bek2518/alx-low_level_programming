e1fa00b6142d90800f9ef002add53e1ba2fc0592
bek2518 <youbereketb20@@hotmail.com>
2022-07-09 08:11:54 -0700

    Prints traingle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_triangle - prints a triangle
+ * print_triangle - prints a triangle with size
  * @size: The size of the triangle
  */
 void print_traingle(int size)
