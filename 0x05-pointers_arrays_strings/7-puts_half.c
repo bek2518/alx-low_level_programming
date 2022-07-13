7c63b449a94b641c8955d387388032ae288a9ccb
bek2518 <youbereketb20@@hotmail.com>
2022-07-12 19:42:05 -0700

    Prints half a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,9 +8,10 @@ void puts_half(char *str)
 {
 	int i, j;
 
-	for (i = 0; str[i] != 0; i++);
+	for (i = 0; str[i] != 0; i++)
+		;
 
-	j = (i - 1) / 2;
+	j = (i + 1) / 2;
 
 	for (; str[j] != 0; j++)
 	{
