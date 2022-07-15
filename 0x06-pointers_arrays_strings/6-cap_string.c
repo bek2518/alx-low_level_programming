387be0ab32800dbfc4c5d0f4c9d843f8b7cfb455
bek2518 <youbereketb20@@hotmail.com>
2022-07-15 00:30:52 -0700

    Capitalized all words

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,16 +8,24 @@
 
 char *cap_string(char *s)
 {
-	int i;
+	int count = 0, i;
+	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	if (s[0] >= 'a' && s[0] <= 'z')
-		s[0] = s[0] - ' ';
-	for (i = 1; s[i] != '\0'; i++)
+	if (*(s + count) >= 97 && *(s + count) <= 122)
+		*(s + count) = *(s + count) - 32;
+	count++;
+	while (*(s + count) != '\0')
 	{
-		if ((s[i - 1] == ' ' || s[i - 1] == '\n' || s[i - 1] == '\t' || s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"' || s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' || s[i - 1] == '}' || s[i - 1] == '.') && (s[i] > 'a' && s[i] < 'z'))
+		for (i = 0; i < 13; i++)
 		{
-			s[i] = s[i] - ' ';
+			if (*(s + count) == sep_words[i])
+			{
+				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+					*(s + (count + 1)) = *(s + (count + 1)) - 32;
+				break;
+			}
 		}
+		count++;
 	}
 	return (s);
 }
