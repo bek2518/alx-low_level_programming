e00e1524d3842eb218947e04d9b0147a99fc7674
bek2518 <youbereketb20@@hotmail.com>
2022-07-21 01:44:00 -0700

    Returns 1 if a prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,16 +1,34 @@
 #include "main.h"
 
 /**
+ * helper - detects if an input is a prime number
+ * @n: input number
+ * @c: helper
+ * Return: 1 if prime number otherwise 0
+ */
+int helper(unsigned int n, unsigned int c)
+{
+	if (n % c == 0)
+	{
+		if (n == c)
+			return (1);
+		else
+			return (0);
+	}
+	return (0 + helper(n, c + 1));
+}
+/**
  * is_prime_number - if input is prime number or not
  * @n: integer
  * Return: 1 if integer, otherwise 0
  */
 int is_prime_number(int n)
 {
-	if (n % 2 == 0)
+	if (n == 0)
 		return (0);
-	else if (n = 1)
-		return (1);
-	else
-		return (1);
+	if (n < 1)
+		return (0);
+	if (n == 1)
+		return (0);
+	return (helper(n, 2));
 }
