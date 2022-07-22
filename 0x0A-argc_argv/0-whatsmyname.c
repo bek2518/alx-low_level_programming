2cbb75a807dbe46e62a2079572cee2970869b9dc
bek2518 <youbereketb20@@hotmail.com>
2022-07-22 10:25:30 -0700

    Prints its own name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argv: string of commandline arguments
  * Return: 0
  */
-int main (int argc __attribute__((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
