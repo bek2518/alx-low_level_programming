c2918d08162367ba1841ee6542e2c50509cd0277
bek2518 <youbereketb20@@hotmail.com>
2022-07-26 05:30:06 -0700

    Allocates new space in memory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include  <stdlib.h>
+
+/** 
+ * _strdup - allocates new space in memory
+ * @str: string
+ * Return: pointer to an new string or NULL
+ */
+char *_strdup(char *str)
+{
+	char *strout;
+	unsigned int i, j;
+
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	strout = (char *)malloc(sizeof(char) * (i + 1));
+
+	if (strout == NULL)
+		return (NULL);
+	for (j = 0; j <= i; j++)
+		strout[j] = str[j];
+	return (strout);
+}
