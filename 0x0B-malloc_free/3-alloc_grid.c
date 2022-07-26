75f185c1079e4a7f368e8ac59c088fbe856cbe72
bek2518 <youbereketb20@@hotmail.com>
2022-07-26 06:10:23 -0700

    Two dimensional array of integers

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,7 +12,7 @@ int **alloc_grid(int width, int height)
 	int **grid;
 	int i, j;
 
-	if (width < 1|| height < 1)
+	if (width < 1 || height < 1)
 		return (NULL);
 	grid = malloc(height * sizeof(int *));
 
