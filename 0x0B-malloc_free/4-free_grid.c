8822382d20386dd2a3a1a3840040eed1c79fc563
bek2518 <youbereketb20@@hotmail.com>
2022-07-26 06:29:51 -0700

    Frees 2 dimensional grid

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * alloc_grid - frees a 2 dimensional grid previously
+ * free_grid - frees a 2 dimensional grid previously
  * created by alloc_grid
  * @grid: multidimension array of integers
  * @height: height of the grid
