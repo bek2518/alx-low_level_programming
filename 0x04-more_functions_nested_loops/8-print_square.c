ae38a070fe6c070dfb6de449ce50ea380f6a8d3f
bek2518 <bereketb20@hotmail.com>
2022-09-16 18:18:13 +0300

    Removed trailing white space

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_square- Prints a square
- *  @size: Dimension of the square
+ * print_square - Prints a square
+ * @size: Dimension of the square
  */
 void print_square(int size)
 {
