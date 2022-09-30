8ff5c3c866664a0e90dc14e4a63bec1425050091
bek2518 <bereketb20@hotmail.com>
2022-09-30 17:03:14 +0300

    Encode using rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string into rot13
+ * @s: string input
+ * Return: dest pointer
+ */
+
+char *rot13(char *s)
+{
+	int count = 0, i;
+	char letters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot[] = "nopqestuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	while (s[count] != '\0')
+	{
+		for (i = 0; i < 52; i++)
+		{
+			if (s[count] == letters[i])
+			{
+				s[count] = rot[i];
+				break;
+			}
+		}
+		count++;
+	}
+	return (s);
+}
