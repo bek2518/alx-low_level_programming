535607034239e36ac99768856e6ef6017aed08b0
bek2518 <bereketb20@hotmail.com>
2022-10-01 05:41:12 +0300

    Added null

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,6 +9,12 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	int i = 0;
 
+	if (h == NULL)
+		return (i);
+	
+	while (h->prev != NULL)
+		h = h->prev;
+
 	if (h != NULL)
 	{
 		printf("%d\n", h->n);
