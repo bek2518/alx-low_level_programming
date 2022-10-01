0fc93aefc072928f15f4662846b762f0908af025
bek2518 <bereketb20@hotmail.com>
2022-10-01 05:44:02 +0300

    Minor change

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,19 +7,13 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	int i = 0;
+	size_t i = 0;
 
-	if (h == NULL)
-		return (i);
-	
-	while (h->prev != NULL)
-		h = h->prev;
-
-	if (h != NULL)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		i++;
 		h = h->next;
+		i++;
 	}
 	return (i);
 }
