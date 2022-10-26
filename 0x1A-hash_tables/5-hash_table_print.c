772e45c406ab3c6f9b65715f6604f2acd35207ce
bek2518 <bereketb20@hotmail.com>
2022-10-26 18:42:44 +0300

    Added separators

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -8,20 +8,40 @@ void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
 	hash_node_t *node;
+	char *sep;
 
 	if (ht == NULL)
 		return;
 
 	printf("{");
-
+	sep = "";
 	for (i = 0; i < ht->size; i++)
 	{
 		node = ht->array[i];
 		while (node != NULL)
 		{
-			printf("'%s': '%s'", node->key, node->value);
+			printf("%s'%s': '%s'", sep, node->key, node->value);
+			sep = ", ";
 			node = node->next;
 		}
 	}
 	printf("}\n");
+}
+
+
+int main(void)
+{
+    hash_table_t *ht;
+
+    ht = hash_table_create(1024);
+    hash_table_print(ht);
+    hash_table_set(ht, "c", "fun");
+    hash_table_set(ht, "python", "awesome");
+    hash_table_set(ht, "Bob", "and Kris love asm");
+    hash_table_set(ht, "N", "queens");
+    hash_table_set(ht, "Asterix", "Obelix");
+    hash_table_set(ht, "Betty", "Cool");
+    hash_table_set(ht, "98", "Battery Street");
+    hash_table_print(ht);
+    return (EXIT_SUCCESS);
 }
