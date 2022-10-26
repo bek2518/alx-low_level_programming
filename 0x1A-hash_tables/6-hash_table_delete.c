ba077e72ce1398e1176b5f008fbabeecbdfe1bcd
bek2518 <bereketb20@hotmail.com>
2022-10-26 18:47:01 +0300

    Delete hash table

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node1;
+	hash_node_t *node2;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		node1 = ht->array[i];
+		while ((node2 = node1) != NULL)
+		{
+			node1 = node1->next;
+			free(node2->key);
+			free(node2->value);
+			free(node2);
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
