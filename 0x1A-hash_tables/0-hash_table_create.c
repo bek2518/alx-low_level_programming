79184054a6c9b242888d34d57ef25c9b5a755cac
bek2518 <bereketb20@hotmail.com>
2022-10-26 13:00:10 +0300

    Minor edit

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,10 +1,10 @@
 #include "hash_tables.h"
+
 /**
  * hash_table_create - function that creates a new hastable
  * @size: size of the table
  * Return: pointer to the new table
  */
-
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
