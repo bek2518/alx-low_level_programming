8cf9bc835863457e50fb06d85b87b0dde64dfc2c
bek2518 <bereketb20@hotmail.com>
2022-10-26 18:21:08 +0300

    Revert

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -18,13 +18,13 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 
 	if (key == NULL || *key == '\0')
 		return (NULL);
-
-/**	if (ht->array[index] == NULL)
+	
+	if (ht->array[index] == NULL)
 		return (NULL);
-
+	
 	if (strcmp(ht->array[index]->key, key) == 0)
 		return (ht->array[index]->value);
-*/
+
 	node = ht->array[index];
 
 	while (node != NULL)
