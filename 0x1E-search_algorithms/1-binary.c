998fa2239fab8f3a7547dc07af5e854b16497c4e
bek2518 <bereketb20@hotmail.com>
2023-01-29 11:00:12 +0300

    Binary search and Big O

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,45 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for value in an array of integers
+ * using binary search algorithm
+ * @array: pointer to the first element of the array to search
+ * sorted in ascending order
+ * @size: size of the array
+ * @value: value to search for
+ * Return: first index where value is located
+*/
+
+int binary_search(int *array, size_t size, int value)
+{
+	size_t left, right, middle, i;
+
+	left = 0;
+	right = size - 1;
+
+	while (left <= right)
+	{
+		printf("Searching in array: ");
+		for (i = left; i <= right; i++)
+		{
+			if (i < right)
+				printf("%ld, ", i);
+			else
+				printf("%ld\n", i);
+		}
+		middle = (left + right) / 2;
+		if (array[middle] == value)
+		{
+			return (middle);
+		}
+		else if (array[middle] < value)
+		{
+			left = middle + 1;
+		}
+		else
+		{
+			right = middle - 1;
+		}
+	}
+	return (-1);
+}
